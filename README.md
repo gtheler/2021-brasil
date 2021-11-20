e52c0a728a0f9ed5354bbb551d889b856f636ecd
gtheler <jeremy@seamplex.com>
2021-11-20 09:05:57 -0300

    initial

---
+++ README.md
@@ -0,0 +1,30 @@
+---
+title: |
+ | A free and open source computational tool for solving
+ | (nuclear-related) differential equations in the cloud
+author: German (a.k.a. Jeremy) Theler
+email: gtheler@seamplex.com
+institute: |
+ | International Nuclear Atlanctic Conference
+ | Round Table 4 ENFIR Best Estimate codes and methods
+ | for Reactor Physics, Thermal Fluid Dynamics and Fuel Behavior
+aspectratio: 169
+lang: en-US
+theme: default
+innertheme: rectangles
+fonttheme: professionalfonts
+outertheme: number
+colorlinks: true
+sansfont: Carlito
+monofont: DejaVuSansMono
+header-includes: \include{syntax.tex}
+...
+
+
+# Why $\rightarrow$ How $\rightarrow$ What
+
+
+![](sinek.jpg)
+
+<https://www.youtube.com/watch?v=u4ZoJKF_VuA>
+
---
+++ pdf.sh
@@ -0,0 +1,31 @@
+hash=$(git rev-parse --short HEAD)
+headepoch=$(git log -1 --format="%ct")
+date="${hash}---$(LC_ALL=${DATELC} date -d@${headepoch} +%Y-%m-%d)"
+if [ ! -z "$(git status --porcelain)" ]; then
+  date="${date}+"
+fi
+
+cat << EOF > hash.yaml
+---
+date: ${date}
+...
+EOF
+
+pandoc -s -f markdown+smart -t beamer+smart hash.yaml README.md -o README.pdf \
+  -M handout:false \
+  --lua-filter=include-code-files.lua \
+  --lua-filter=code-style.lua \
+  --listings \
+  --pdf-engine=xelatex \
+  --template=template.tex
+
+# pandoc -s -f markdown+smart -t beamer+smart hash.yaml README.md -o README-handout.pdf \
+#   -M handout:true \
+#   --lua-filter=include-code-files.lua \
+#   --lua-filter=code-style.lua \
+#   --listings \
+#   --pdf-engine=xelatex \
+#   --template=template.tex
+  
+
+# convert -density 305 README.pdf  pepe.png
